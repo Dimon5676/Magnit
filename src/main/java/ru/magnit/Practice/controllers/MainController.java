448541d4e0f52e97e8c2c4728613de26f447c9c7
Dimon5676 <Dimon5676@yandex.ru>
2020-06-10 23:03:35 +0300

    Added home page

---
+++ src/main/java/ru/magnit/Practice/controllers/MainController.java
@@ -0,0 +1,15 @@
+package ru.magnit.Practice.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String homePage(Model model) {
+
+        return "home";
+    }
+}
