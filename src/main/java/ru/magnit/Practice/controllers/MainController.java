3b873313f47fad38ba450534eb370bc86544bd18
Dimon5676 <Dimon5676@yandex.ru>
2020-06-14 01:04:38 +0300

    Added counter

--- src/main/java/ru/magnit/Practice/controllers/MainController.java
+++ src/main/java/ru/magnit/Practice/controllers/MainController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+    private static int count = 0;
+
     @GetMapping("/")
     public String homePage(Model model) {
-
+        count++;
+        model.addAttribute("count", count);
         return "home";
     }
 }
