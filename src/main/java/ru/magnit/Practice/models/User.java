6d643898b51885b829d3855ea45709a4b5baf4cb
Dimon5676 <Dimon5676@yandex.ru>
2020-06-24 18:55:18 +0300

    Added constructors and getters, setters

--- src/main/java/ru/magnit/Practice/models/User.java
+++ src/main/java/ru/magnit/Practice/models/User.java
@@ -15,4 +15,56 @@ public class User {
     private String lastName;
     private String middleName;
     private String email;
+
+    public User() {
+
+    }
+
+    public User(Long id, String name, String lastName, String middleName, String email) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.middleName = middleName;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
