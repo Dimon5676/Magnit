51a12e514a1878375566ad9193cdeac6d365c771
Dimon5676 <Dimon5676@yandex.ru>
2020-06-30 06:57:45 +0300

    Added sorting in controller

--- src/main/java/ru/magnit/Practice/controllers/CatalogController.java
+++ src/main/java/ru/magnit/Practice/controllers/CatalogController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.magnit.Practice.models.Idea;
 import ru.magnit.Practice.repos.IdeaRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class CatalogController {
@@ -14,8 +18,23 @@ public class CatalogController {
     IdeaRepository ideaRepository;
 
     @GetMapping("/catalog")
-    public String catalog(Model model){
-        Iterable<Idea> ideas = ideaRepository.findAll ();
+    public String catalog(
+            @RequestParam(required = false) String sort,
+            @RequestParam(required = false) String filter,
+            Model model
+    ){
+        Iterable<Idea> ideas = ideaRepository.findAll();
+        if (sort.equalsIgnoreCase("alphabet")) {
+            //Сортировка по ideas по алфавиту
+        }
+
+        if (sort.equalsIgnoreCase("rate")) {
+            //Сортировка по ideas по популярности
+        }
+
+        if (sort.equalsIgnoreCase("date")) {
+            //Сортировка по ideas по дате
+        }
         model.addAttribute ("ideas", ideas);
         return "catalog";
     }
